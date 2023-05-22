62936f40e5c99bd7e85ee6bbdf32e25a2d08352e
Artbaddon <sebarsomo@gmail.com>
2023-05-22 16:25:07 -0500

    added

--- shoe-store/config/constants.php
+++ shoe-store/config/constants.php
@@ -10,11 +10,6 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'shoe-order');
 
 
-$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD); //Database Conection
-$db_select = mysqli_select_db($conn, 'DB_NAME'); //Selecting Database
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_connect_error()); //Database Conection
+$db_select = mysqli_select_db($conn, 'DB_NAME') or die(mysqli_connect_error()); //Selecting Database
 
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
