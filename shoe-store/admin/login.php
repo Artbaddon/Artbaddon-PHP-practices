6ded284dbfea536be3ca41c8ca4a134c67825cc6
Artbaddon <sebarsomo@gmail.com>
2023-05-27 17:25:49 -0500

    new improves

--- shoe-store/admin/login.php
+++ shoe-store/admin/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,17 +8,47 @@
     <title>Login - Shoe Order System -</title>
     <link rel="stylesheet" href="../css/admin.css">
 </head>
-<body>
+
+<body class="body-login">
     <div class="login-container">
         <div class="wrapper-login">
+            <h1 class="text-center">Admin Login</h1>
             <div class="login">
-                <h1>hola    </h1>
+
+
+                <!-- Login Form Starts Here -->
+                <form action="" method="POST" class="text-center">
+
+                    <input type="text" name="username" placeholder="Enter Username"><br /><br />
+
+                    <input type="password" name="password" placeholder="Enter Password"><br /><br />
+
+                    <input type="submit" value="login" class="btn-primary login-btn">
+                </form>
             </div>
         </div>
-        
+
 
     </div>
 
-    
+
 </body>
+
 </html>
+<?php
+// Checked whether the submit button is clicked or not
+
+if (isset($_POST['submit'])) {
+
+    // 1. Get the Data from login form]
+    echo "aaaa";
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // 2. SQL to check whether the user with username and password exist or not
+
+    $sql="SELECT * FROM tlb_admin WHERE username='$username' AND password='password'";
+
+    // 4. Execute the query
+}
+?>
